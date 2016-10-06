5c7c2cf2e7a11a5176adfdf473b512f350f42386
Santi Puch Giner <santipuch590@gmail.com>
2016-10-06 23:31:59 +0200

    Added form factor computation

--- AnalyzeDataset.m
+++ AnalyzeDataset.m
@@ -1,9 +1,9 @@
-%% CONSTANTS 
+%% CONSTANTS
 TRAIN_DATASET_PATH = './DataSetDelivered/train';
 do_readDataset=1;
-do_maxmin=0;
-do_formfactor=0;
-do_fillingratio=1;
+do_maxmin=1;
+do_formfactor=1;
+do_fillingratio=0;
 do_freqappearance=0;
 do_signalgrouping=0;
 %% READ DATASET
@@ -21,7 +21,9 @@ end
 %% FORM FACTOR
 if do_formfactor
     disp('Computing form factor...');
-    form_fac = form_factor(train_dataset);
+    [form_factor_list, ...
+    form_factor_A, form_factor_B, form_factor_C, ...
+    form_factor_D, form_factor_E, form_factor_F] = form_factor(train_dataset);
 end
 %% FILLING RATIO
 if do_fillingratio
--- dataset_analysis/form_factor.m
+++ dataset_analysis/form_factor.m
@@ -1,8 +1,43 @@
-function [ output_args ] = form_factor( train_dataset )
-%FORM_FACTOR Summary of this function goes here
-%   Detailed explanation goes here
+function [ form_factor_list, ...
+           form_factor_A, form_factor_B, form_factor_C, ...
+           form_factor_D, form_factor_E, form_factor_F ] = form_factor( train_dataset )
+%FORM_FACTOR Form factor (width / height) for all signals
 
-output_args = 'TO BE IMPLEMENTED';
+form_factor_list = [];
+form_factor_A = [];
+form_factor_B = [];
+form_factor_C = [];
+form_factor_D = [];
+form_factor_E = [];
+form_factor_F = [];
 
+% Iterate over all the dataset
+for i=1:length(train_dataset)
+    [bound_box, type, num_elems] = parse_annotations(train_dataset(i).annotations);
+    for m=1:num_elems
+        c1=bound_box(m, 1);
+        c2=bound_box(m, 2);
+        c3=bound_box(m, 3);
+        c4=bound_box(m, 4);
+        types=type{m};
+        form_factor_value = (c3 - c1) / (c4 - c2);
+        form_factor_list = [form_factor_list, form_factor_value];
+        switch types
+            case 'A'
+                form_factor_A = [form_factor_A, form_factor_value];
+            case 'B'
+                form_factor_B = [form_factor_B, form_factor_value];
+            case 'C'
+                form_factor_C = [form_factor_C, form_factor_value];
+            case 'D'
+                form_factor_D = [form_factor_D, form_factor_value];
+            case 'E'
+                form_factor_E = [form_factor_E, form_factor_value];
+            case 'F'
+                form_factor_F = [form_factor_F, form_factor_value];
+            otherwise
+                % Do nothing
+        end
+    end
 end
 
