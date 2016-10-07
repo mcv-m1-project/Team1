c6cd8e4df377eee48bdb3ca9e0f4a1fa26661dc1
ArantxaCasanova <ar.casanova.8@gmail.com>
2016-10-07 11:54:47 +0200

    .

--- signal_characteristics.m
+++ signal_characteristics.m
@@ -9,12 +9,21 @@ end
 function freqApp (input_dir)
 
 list= dir([input_dir '/train/gt'])
-
+arr_types=[];
 for i=3: size(list,1)
      list(i).name
  [c1,c2,c3,c4,type]=   textread([input_dir '/train/gt/' list(i).name], '%s %s %s %s %s');
- if(ismember(type,arr_types))
-     arr_types....
-end
+ if(~ismember(type,arr_types))
+     if (isempty(arr_types))
+         arr_types=[type];
+     else
+         arr_types=[arr_types type];
+     end
+ end
+ end
+
+arr_types
 
 end
+
+
