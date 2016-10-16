4a5c48ccf96aa39c3ecd5973c14f03507f923fb0
bluque <luquelopez.belen@gmail.com>
2016-10-16 12:17:44 +0200

    Fix in histogram axes

--- histograms/calculateTrainHists.m
+++ histograms/calculateTrainHists.m
@@ -103,22 +103,22 @@ if plotHist
     %plot histograms
     figure('name', 'signal type A, B & C');
     bar3(histoABC)
-    xlabel('1st comp'); ylabel('2nd comp');
+    xlabel('2nd comp'); ylabel('1st comp');
     set(get(gca,'child'),'FaceColor','interp','CDataMode','auto');
     
     figure('name', 'signal type D & F');
     bar3(histoDF)
-    xlabel('1st comp'); ylabel('2nd comp');
+    xlabel('2nd comp'); ylabel('1st comp');
     set(get(gca,'child'),'FaceColor','interp','CDataMode','auto');
     
     figure('name', 'signal type E');
     bar3(histoE)
-    xlabel('1st comp'); ylabel('2nd comp');
+    xlabel('2nd comp'); ylabel('1st comp');
     set(get(gca,'child'),'FaceColor','interp','CDataMode','auto');
     
     figure('name', 'All signal types');
     bar3(pdf)
-    xlabel('1st comp'); ylabel('2nd comp');
+    xlabel('2nd comp'); ylabel('1st comp');
     set(get(gca,'child'),'FaceColor','interp','CDataMode','auto');
     
 end
