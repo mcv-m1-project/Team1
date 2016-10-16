29edaea1db6dc650915f6e099bf39a0139f57a54
Belén <Belen@MacBook-Air-de-Belen.local>
2016-10-16 12:17:44 +0200

    Fix in histogram axes

--- histograms/viewTrainHistograms.m
+++ histograms/viewTrainHistograms.m
@@ -8,27 +8,24 @@ histoABC = hist_individual{1};
 histoDF = hist_individual{2};
 histoE = hist_individual{3};
 
-figure('name', 'All signs histogram normalized');
-bar3(histAll)
-
 figure('name', 'signal type A, B & C');
 bar3(histoABC)
-xlabel('1st comp'); ylabel('2nd comp');
+xlabel('2nd comp'); ylabel('1st comp');
 set(get(gca,'child'),'FaceColor','interp','CDataMode','auto');
 
 figure('name', 'signal type D & F');
 bar3(histoDF)
-xlabel('1st comp'); ylabel('2nd comp');
+xlabel('2nd comp'); ylabel('1st comp');
 set(get(gca,'child'),'FaceColor','interp','CDataMode','auto');
 
 figure('name', 'signal type E');
 bar3(histoE)
-xlabel('1st comp'); ylabel('2nd comp');
+xlabel('2nd comp'); ylabel('1st comp');
 set(get(gca,'child'),'FaceColor','interp','CDataMode','auto');
 
 figure('name', 'All signal types');
 bar3(histAll)
-xlabel('1st comp'); ylabel('2nd comp');
+xlabel('2nd comp'); ylabel('1st comp');
 set(get(gca,'child'),'FaceColor','interp','CDataMode','auto');
 
 
