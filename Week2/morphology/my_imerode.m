573c11b7704500c3e958c52edcf50a3ecf269770
Anna Martí <annamartiaguilera@gmail.com>
2016-10-18 23:18:19 +0200

    Morphology operators update

--- Week2/morphology/my_imerode.m
+++ Week2/morphology/my_imerode.m
@@ -2,7 +2,7 @@ function img_er=my_imerode(img, se)
 
 padx=ceil(size(se,1)/2);
 pady=ceil(size(se,2)/2);
-img=padarray(img,[padx pady]);
+img=padarray(img,[padx pady],'symmetric');
 img_er=img;
 for x=1+padx:size(img,1)-padx
     for y=1+pady:size(img,2)-pady
@@ -11,13 +11,14 @@ for x=1+padx:size(img,1)-padx
             i=a-padx;
             for b=1:size(se,2)
                 j=b-pady;
-                if(se(a,b)==1)&&(img(x+i,y+j)<min_se)
+                if(se(a,b)>0)&&(img(x+i,y+j)<min_se)
                     min_se=img(x+i,y+j);
                 end
             end
         end
         if img(x,y)>min_se
             img_er(x,y)=min_se;
+            
         end
     end
 end
