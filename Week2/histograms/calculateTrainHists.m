106e4ed2ac23f8a6fe3c9524ad7aa5a1c7df1c32
bluque <luquelopez.belen@gmail.com>
2016-10-18 21:07:18 +0200

    Best histogram search

--- Week2/histograms/calculateTrainHists.m
+++ Week2/histograms/calculateTrainHists.m
@@ -1,4 +1,4 @@
-function calculateTrainHists(colorsp)
+function [histoABC,histoDF,histoE,histAll] = calculateTrainHists(colorsp,Nbins,perc,deletegrays)
 close all
 DATASET_PATH = 'DataSetDelivered';
 %READ DATASET
@@ -8,7 +8,7 @@ TRAIN_DATASET_PATH = fullfile(DATASET_PATH, 'train');
 train_dataset = read_train_dataset(TRAIN_DATASET_PATH, train_split);
 
 saveHist = false;
-plotHist = true;
+plotHist = false;
 
 masked_comp_ABC=[];
 masked_comp_DF=[];
@@ -38,9 +38,6 @@ for i=1: size(train_dataset,2)
         comp=im_cs(:,:,2);
         comp2=im_cs(:,:,3);
     end
-    %take the first 2 components of the image in the new color space
-    comp=im_cs(:,:,2);
-    comp2=im_cs(:,:,3);
     
     %parse annotations to get signal type
     [bound_box, type, num_elems] = parse_annotations(train_dataset(i).annotations);
@@ -71,8 +68,8 @@ end
 
 if strcmp(colorsp,'hsv')
     %n for HSV
-    n1=0:1/63:1;
-    n2=0:1/63:1;
+    n1=0:1/(Nbins-1):1;
+    n2=0:1/(Nbins-1):1;
 elseif strcmp(colorsp,'ycrcb')
     %n for YCRCB
     n1=0:3.5:240;
@@ -84,18 +81,35 @@ edges = {n1; n2};
 
 %calculate the histogram with the 2 first components
 histoABC= hist3([masked_comp_ABC',masked_comp2_ABC'],'Edges', edges);
-
 histoDF= hist3([masked_comp_DF',masked_comp2_DF'],'Edges', edges);
-
 histoE= hist3([masked_comp_E',masked_comp2_E'],'Edges', edges);
 
+if deletegrays == true
+    % %Eliminate low-saturated values (25%)
+    histoABC(:,1:round(perc*Nbins))=0;
+    histoDF(:,1:round(perc*Nbins))=0;
+    histoE(:,1:round(perc*Nbins))=0;
 
-pdf=hist3([masked_comp_total' , masked_comp2_total'],'Edges',edges);
+    %Eliminate non-red (8%-85%)or non-blue(0-55%,65%-100) values
+    histoABC(round(0.08*Nbins):round(0.85*Nbins),:)=0;
+
+    histoDF(1:round(0.55*Nbins),:)=0;
+    histoDF(round(0.65*Nbins):Nbins,:)=0;
+
+    %Eliminate non-red or non-blue values(8%-55%,65%-85%) 
+    histoE(round(0.08*Nbins):1:round(0.55*Nbins),:)=0;
+    histoE(round(0.65*Nbins):round(0.85*Nbins),:)=0;
+
+end
+
+histAll = histoABC+histoDF+histoE;
+
+%pdf=hist3([masked_comp_total' , masked_comp2_total'],'Edges',edges);
 
 if saveHist
 
     %store histograms
-    save(['DataSetDelivered/HistALL_', colorsp, '.mat'],'pdf');
+    %save(['DataSetDelivered/HistALL_', colorsp, '.mat'],'pdf');
     save(['DataSetDelivered/HistABC_', colorsp, '.mat'],'histoABC');
     save(['DataSetDelivered/HistDF_', colorsp, '.mat'],'histoDF');
     save(['DataSetDelivered/HistE_', colorsp, '.mat'],'histoE');
@@ -105,22 +119,22 @@ end
 if plotHist
     %plot histograms
     figure('name', 'signal type A, B & C');
-    bar3(histoABC)
+    bar3(histoABC);
     xlabel('2nd comp'); ylabel('1st comp');
     set(get(gca,'child'),'FaceColor','interp','CDataMode','auto');
     
     figure('name', 'signal type D & F');
-    bar3(histoDF)
+    bar3(histoDF);
     xlabel('2nd comp'); ylabel('1st comp');
     set(get(gca,'child'),'FaceColor','interp','CDataMode','auto');
     
     figure('name', 'signal type E');
-    bar3(histoE)
+    bar3(histoE);
     xlabel('2nd comp'); ylabel('1st comp');
     set(get(gca,'child'),'FaceColor','interp','CDataMode','auto');
     
     figure('name', 'All signal types');
-    bar3(pdf)
+    bar3(histAll)
     xlabel('2nd comp'); ylabel('1st comp');
     set(get(gca,'child'),'FaceColor','interp','CDataMode','auto');
     
