3b2ac267046887b28b4fecd19c47ffcc11c4ca6b
ArantxaCasanova <ar.casanova.8@gmail.com>
2016-10-20 16:25:57 +0200

    task 2.2
    Minor fixes, manly displays

--- morphology/my_imbothat.m
+++ morphology/my_imbothat.m
@@ -1,9 +1,5 @@
 function img_bh=my_imbothat(img, se)
 
 img_bh=abs(my_imclose(img,se)-img);
-
-size(img)
-size(my_imclose(img,se))
-
 end
 
--- morphology/my_imtophat.m
+++ morphology/my_imtophat.m
@@ -1,8 +1,5 @@
 function img_th=my_imtophat(img, se)
 
 img_th=abs(img-my_imopen(img,se));
-size(img)
-size(my_imopen(img,se))
-
 end
 
