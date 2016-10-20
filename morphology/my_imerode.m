68b1e307f3a7d1831242150c66b28cc4d0ecacbe
Anna Martí <annamartiaguilera@gmail.com>
2016-10-20 19:56:59 +0200

    morphology optimizaztion

--- morphology/my_imerode.m
+++ morphology/my_imerode.m
@@ -4,21 +4,50 @@ padx=ceil(size(se,1)/2);
 pady=ceil(size(se,2)/2);
 img=padarray(img,[padx pady],'symmetric');
 img_er=img;
-for x=1+padx:size(img,1)-padx
-    for y=1+pady:size(img,2)-pady
-        min_se=255;
-        for a=1:size(se,1)
-            i=a-padx;
-            for b=1:size(se,2)
-                j=b-pady;
-                if(se(a,b)==1)&&(img(x+i,y+j)<min_se)
-                    min_se=img(x+i,y+j);
+sel=strel(se);
+seq=getsequence(sel);
+seqsize=size(seq,1);
+if seqsize>1
+    for sz=1:seqsize
+        se=seq(sz,1).Neighborhood;
+        for x=1+padx:size(img,1)-padx
+            for y=1+pady:size(img,2)-pady
+                min_se=255;
+                for a=1:size(se,1)
+                    i=a-padx;
+                    for b=1:size(se,2)
+                        j=b-pady;
+                        if(se(a,b)==1)&&(img(x+i,y+j)<min_se)
+                            min_se=img(x+i,y+j);
+                        end
+                    end
+                end
+                if img(x,y)>min_se
+                    img_er(x,y)=min_se;
+
                 end
             end
         end
-        if img(x,y)>min_se
-            img_er(x,y)=min_se;
-            
+        
+        
+    end
+else
+    for x=1+padx:size(img,1)-padx
+        for y=1+pady:size(img,2)-pady
+            min_se=255;
+            for a=1:size(se,1)
+                i=a-padx;
+                for b=1:size(se,2)
+                    j=b-pady;
+                    if(se(a,b)==1)&&(img(x+i,y+j)<min_se)
+                        min_se=img(x+i,y+j);
+                    end
+                end
+            end
+            if img(x,y)>min_se
+                img_er(x,y)=min_se;
+
+            end
         end
     end
 end
