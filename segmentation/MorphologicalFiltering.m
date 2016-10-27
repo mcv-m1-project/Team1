d7c88abb308b3a0c1a6f5a340cb38068c1487266
ArantxaCasanova <ar.casanova.8@gmail.com>
2016-10-27 09:22:13 +0200

    New morphology
    Measures in train:
    Precision: 0.283030
    Recall: 0.716462
    F measure: 0.405767
    
    Measures in val:
    
    Precision: 0.251168
    Recall: 0.742584
    F measure: 0.375372

--- segmentation/MorphologicalFiltering.m
+++ segmentation/MorphologicalFiltering.m
@@ -11,7 +11,7 @@ DEFAULT_AO_SIZE_1 = 300;
 % DEFAULT AREA OPENING OBJECT SIZE (METHOD 2)
 DEFAULT_AO_SIZE_2 = 5000;
 % DEFAULT METHOD
-DEFAULT_METHOD = 1;
+DEFAULT_METHOD = 3;
 
 
 
@@ -30,14 +30,16 @@ if nargin < 2
 end
 
 switch(method)
+   
     case 1
         % Area opening
-        pixelCandidates = bwareaopen(pixelCandidates, small_obj_thr_1);
-
-        % Dilation to connect disconnected components that survived the area
+         pixelCandidates = bwareaopen(pixelCandidates, small_obj_thr_1);
+       % Dilation to connect disconnected components that survived the area
         % opening
         SE = strel('diamond', strel_R);
         pixelCandidates = imdilate(pixelCandidates, SE);
+        pixelCandidates = imfill(pixelCandidates, 'holes');
+   
     case 2
         % Dilation to connect disconnected components that survived the area
         % opening
@@ -46,12 +48,21 @@ switch(method)
         
         % Area opening
         pixelCandidates = bwareaopen(pixelCandidates, small_obj_thr_2);
+        pixelCandidates = imfill(pixelCandidates, 'holes');
+
+    case 3 
+     %    SE = strel('diamond', strel_R);
+        SE_close=strel('disk',7);
+        SE2=strel('square', 5 );
+        SE_th=strel('square', 250);
+        
+        pixelCandidates= imclose(pixelCandidates, SE_close);
+        pixelCandidates = imfill(pixelCandidates, 'holes');
+        pixelCandidates=imopen(pixelCandidates,SE2);
+        pixelCandidates=imtophat(pixelCandidates, SE_th);
+       
     otherwise
-        error('You must specify one of the two following methods: 1, 2');
+        error('You must specify one of the two following methods: 1, 2, 3');
 end
-
-% Fill holes
-pixelCandidates = imfill(pixelCandidates, 'holes');
-
 end
 
