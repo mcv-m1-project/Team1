50a660a0e891b51d0a627e72434dae28fd5fa917
ArantxaCasanova <ar.casanova.8@gmail.com>
2016-10-29 10:35:16 +0200

    Integral image

--- candidate_generation_window/SlidingWindow.m
+++ candidate_generation_window/SlidingWindow.m
@@ -1,4 +1,4 @@
-function [ windowCandidates ] = SlidingWindow( im, pixelCandidates )
+function [ windowCandidates ] = SlidingWindow( im, pixelCandidates, window_method )
 %SLIDINGWINDOW Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -13,6 +13,10 @@ ymax = 0;
 pix_step=10;
 win_step=-10;
 
+int_im=[];
+if strcmp(window_method,'integral_window') % store integral image
+    int_im=cumsum(cumsum(double(pixelCandidates),2));	
+end
 for id=1:4
     if id==1
         x_range=1:ceil(x/2);
@@ -51,8 +55,10 @@ for id=1:4
                 end
             end
         end
-        for winx=window_maxsize:win_step:window_minsize
+
+         for winx=window_maxsize:win_step:window_minsize
             winy=winx-8;
+            window_area=winx*winy;
             ymaxx=ymax;
             xmaxx=xmax;
             if xmax>size(pixelCandidates,2)-winx
@@ -63,12 +69,33 @@ for id=1:4
             end
             for i=xmin:pix_step:xmaxx
                 for j=ymin:pix_step:ymaxx
-                    bbox=[j, i, j+winy, i+winx];
-                    fr=filling_ratio(bbox, pixelCandidates);
                     row_up=pixelCandidates(j,i:i+winx);
                     col_left=pixelCandidates(j:j+winy,i);
                     row_down=pixelCandidates(j+winy, i:i+winy);
-                    if nnz(row_up) && nnz(col_left) && nnz(row_down)
+                    if nnz(row_up) && nnz(col_left) && nnz(row_down) % if we have white pixels in 3 of 4 sides of the BBox, do further calculations. Otherwise, don't.
+                    
+                        bbox=[j, i, j+winy, i+winx];
+                    if strcmp(window_method,'naive_window')
+                    fr=filling_ratio(bbox, pixelCandidates);
+                    elseif strcmp(window_method,'integral_window')
+                        A=int_im(j+winy,i+winx);
+                        if i-1<1
+                            B=0;
+                        else
+                        B= int_im (j+winy, i-1);
+                        end
+                        if j-1<1
+                            C=0;
+                        else
+                            C=int_im(j-1,i+winx);
+                        end
+                        if j-1>0 && i-1>0 
+                          D=int_im (j-1,i-1);
+                        else
+                           D=0;
+                        end
+                    fr=(A -B - C + D)/window_area;
+                    end
                     %if nnz(col_left) && nnz(row_down)
                         if ((fr>0.47 && fr<0.53) || (fr>0.77 && fr<0.83) || (fr>0.97 && fr<1.05))
                             %box_struct = struct('x', i, 'y', j, 'w', winx, 'h', winy);
