6b6d21858d17c2e86506399b576a35febe66c5af
Santi Puch Giner <santipuch590@gmail.com>
2016-11-09 01:10:20 +0100

    Small changes in CCL and morphology (method 4) to improve results

--- candidate_generation_window/CCLWindow.m
+++ candidate_generation_window/CCLWindow.m
@@ -7,8 +7,11 @@ stats = regionprops(CC, 'BoundingBox');
 N = length(stats);
 windowCandidates = zeros(N, 4);
 for m=1:N
-     bbox = ceil(stats(m).BoundingBox);
-    windowCandidates(m, :) = bbox;
+    bbox = ceil(stats(m).BoundingBox);
+    % Check size is within the established limits
+    if bbox(3) > 30 && bbox(3) < 250 && bbox(4) > 30 && bbox(4) < 250
+        windowCandidates(m, :) = bbox;
+    end
 end
 
 end
