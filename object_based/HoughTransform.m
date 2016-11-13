e0a106e4839e54be773c5558dbab227e1d9761f5
Santi Puch Giner <santipuch590@gmail.com>
2016-11-13 13:52:54 +0100

    Hough refactor

--- object_based/HoughTransform.m
+++ object_based/HoughTransform.m
@@ -2,25 +2,25 @@ function [ windowCandidates ] = HoughTransform(im, pixelCandidates)
 %HOUGHTRANSFORM Summary of this function goes here
 %   Detailed explanation goes here
 
-plotting=true;
+plotting=false;
 
 if plotting
-    tic;    
+    tic;
 end
 
-% extract contours
+% Extract contours
 pc_edges = bwperim(pixelCandidates, 8);
 
-% dilate contour
+% Dilate contour
 se=strel('disk',3);
 pc_edges=imdilate(pc_edges,se);
 
-% thin contour
+% Thin contour
 %pc_edges=bwmorph(pc_edges,'thin','Inf');
 if plotting
-subplot(2,3,1)
-imshow(pixelCandidates)
-title('Input mask')
+    subplot(2,3,1)
+    imshow(pixelCandidates)
+    title('Input mask')
 end
 
 %% Standard Hough Transform
@@ -29,11 +29,6 @@ end
 
 %calculate peaks in HT
 P  = houghpeaks(H,25,'threshold',ceil(0.3*max(H(:))));
-%P  = houghpeaks(H,100);
-%plot peaks in HT
-%x = theta(P(:,2)); y = rho(P(:,1));
-%plot(x,y,'s','color','white');
-%hold off;
 
 %exract lines from HT and its peaks
 lines = houghlines(pc_edges, theta, rho, P, 'FillGap', 50, 'MinLength', 20);
@@ -41,27 +36,26 @@ thetas=[];
 rhos=[];
 
 if plotting
-
-subplot(2,3,2)
-imshow(pc_edges)
-title('Contour image')
-subplot(2,3,3), imshow(pc_edges), hold on
-for k = 1:length(lines)
-    thetas=[thetas lines(k).theta];
-    rhos=[rhos lines(k).rho];
-   xy = [lines(k).point1; lines(k).point2];
-   plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
-
+    
+    subplot(2,3,2)
+    imshow(pc_edges)
+    title('Contour image')
+    subplot(2,3,3), imshow(pc_edges), hold on
+    for k = 1:length(lines)
+        thetas=[thetas lines(k).theta];
+        rhos=[rhos lines(k).rho];
+        xy = [lines(k).point1; lines(k).point2];
+        plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+        
+    end
+    
+    hold off
+    title('Detected hough segments')
+    
 end
-
-hold off
-title('Detected hough segments')
-
-end
-%%%%% HOUGH Heuristics
 
 %% Circular Hough Transform
-% Split in two Hough transforms not to cover a too broad radii range in a 
+% Split in two Hough transforms not to cover a too broad radii range in a
 % single call.
 [circen_1, cirrad_1] = imfindcircles(pixelCandidates, [15, 40], 'Sensitivity', 0.8, 'EdgeThreshold', 0.25);
 [circen_2, cirrad_2] = imfindcircles(pixelCandidates, [40, 120], 'Sensitivity', 0.8, 'EdgeThreshold', 0.25);
@@ -77,7 +71,7 @@ if(~isempty(lines))
     %pool vector is the lines vector positions where a shape has not been
     %assigned yet. Initialized with all the positions.
     pool=1:length(lines);
-
+    
     %segment index to analyse. Initialized with the first index in 'pool'.
     if ~isempty(pool)
         ind=pool(1);
@@ -91,7 +85,7 @@ if(~isempty(lines))
         %relation, it will be a shape with just one segment and,
         %therefore,discarded.
         shape=[shape ind];
-
+        
         %find closest segment ('lines' index) to target segment 'ind'
         [closest_ind,end_seg_]=findClosestPoint(lines,pool,ind,end_seg); %-1 if no close segments
         %theta diference between current index candidate and closest index
@@ -100,15 +94,15 @@ if(~isempty(lines))
         else
             end_seg=2;
         end
-
+        
         %if a close segment fits triangle heuristics, erase previous segment 'ind'
         %from pool.
         pool(find(pool==ind))=[];
-
-        if closest_ind~=-1 
+        
+        if closest_ind~=-1
             theta_diff=abs(ref_theta-lines(closest_ind).theta);
-
-            if(theta_diff<10  || (theta_diff<95 && theta_diff>20)) 
+            
+            if(theta_diff<10  || (theta_diff<95 && theta_diff>20))
                 %take next ind as closest_ind
                 ind=closest_ind;
             else
@@ -129,13 +123,13 @@ if(~isempty(lines))
             j=j+1;
             %restart 'shape'
             shape=[];
-             %if the closest segment doesn't fit in the triangle heuristics, take
+            %if the closest segment doesn't fit in the triangle heuristics, take
             %next segment to consider as the first one in the pool
             if (~isempty(pool))
                 ind=pool(1);
             end
         end
-
+        
         %eliminate shapes without at least 3 segments
         def_shapes={};
         i=1;
@@ -152,7 +146,7 @@ if(~isempty(lines))
     end
     def_shapes_={};
     k=1;
-
+    
     for i=1:length(def_shapes)
         shape=def_shapes{i};
         if plotting
@@ -174,88 +168,92 @@ if(~isempty(lines))
             k=k+1;
         end
     end
-
-
-%% HOUGH Bounding boxes
-
-% Get BB from shape (triangle and square heuristics)
-windowCandidates=[];
-
-disp(['Num shapes after filter shape ' int2str(length(def_shapes_))]);
-if plotting
-%show lines in contour image
-subplot(2,3,4)
-imshow(pc_edges), hold on
-for i=1:length(def_shapes_)
-   vec= def_shapes_{i};
-   if i==1
-   color='green';
-   elseif i==2
-       color='red';
-   elseif i==3
-       color='blue';
-   elseif i==4
-       color='magenta';
-   else
-       color='yellow';
-   end
-for k = 1:length(vec)
-    xy = [lines(vec(k)).point1; lines(vec(k)).point2];
-    plot(xy(:,1),xy(:,2),'LineWidth',2,'Color',color);
+    
+    
+    %% HOUGH rectangle and triangle bounding boxes
+    
+    % Get BB from shape (triangle and square heuristics)
+    windowCandidates=[];
+    for i=1:length(def_shapes_)
+        bb=getBoundingBox(lines,def_shapes_{i});
+        fr= nnz(pixelCandidates(bb(2):bb(2)+bb(4),bb(1):bb(1)+bb(3)))/(bb(3)*bb(4));
+        if (bb(3)/bb(4)<2.3 && bb(3)/bb(4)>0.5 && fr>0.3)
+            windowCandidates=[windowCandidates; bb];
+        end
+    end
+    
+else
+    windowCandidates=[];
 end
-end
-if ~isempty(circen_1) 
- circ1 = viscircles(circen_1, cirrad_1, 'Color', 'b', 'LineWidth', 1, 'LineStyle', '--');
-end
-if ~isempty(circen_2)
-    circ2 = viscircles(circen_2, cirrad_2, 'Color', 'r', 'LineWidth', 1, 'LineStyle', '--');
-end 
-hold off
-title('Shapes in different colors')
-end
-windowCandidates=[];
-%%%%% HOUGH Heuristics
+
 % Get BB from circle centers and radius
 if numel(circen_1) > 0
     for k=1:size(circen_1, 1)
         bb = [circen_1(k, 1) - cirrad_1(k), circen_1(k, 2) - cirrad_1(k), ...
-              cirrad_1(k), cirrad_1(k)];
+            2 * cirrad_1(k), 2 * cirrad_1(k)];
         windowCandidates=[windowCandidates; bb];
-    end   
+    end
 end
 if numel(circen_2) > 0
     for k=1:size(circen_2, 1)
         bb = [circen_2(k, 1) - cirrad_2(k), circen_2(k, 2) - cirrad_2(k), ...
-              2 * cirrad_2(k), 2 * cirrad_2(k)];
+            2 * cirrad_2(k), 2 * cirrad_2(k)];
         windowCandidates=[windowCandidates; bb];
-    end 
+    end
 end
-for i=1:length(def_shapes_)
-   bb=getBoundingBox(lines,def_shapes_{i});
-  fr= nnz(pixelCandidates(bb(2):bb(2)+bb(4),bb(1):bb(1)+bb(3)))/(bb(3)*bb(4));
-   if (bb(3)/bb(4)<2.3 && bb(3)/bb(4)>0.5 && fr>0.3)
-    windowCandidates=[windowCandidates; bb];      
-   end
-end
+
+%% Plotting
 if plotting
-   subplot(2,3,5), scatter(thetas,rhos)
-title('Theta and rho scatter')
-h=subplot(2,3,6); imshow(pixelCandidates,'Parent',h),hold on
-  for zz = 1:size(windowCandidates,1)
-      w=windowCandidates;
-      r=rectangle(h,'Position', [w(zz,1), w(zz,2), w(zz,3), w(zz,4)], 'LineWidth',1,'EdgeColor','b');
-               
-  end   
-  title('Final bounding boxes without merge')
-  drawnow
+    subplot(2,3,4)
+    imshow(pc_edges), hold on
+    if exist('def_shapes_', 'var')
+        disp(['Num shapes after filter shape ' int2str(length(def_shapes_))]);
+        % Show lines in contour image
+        for i=1:length(def_shapes_)
+            vec= def_shapes_{i};
+            if i==1
+                color='green';
+            elseif i==2
+                color='red';
+            elseif i==3
+                color='blue';
+            elseif i==4
+                color='magenta';
+            else
+                color='yellow';
+            end
+            for k = 1:length(vec)
+                xy = [lines(vec(k)).point1; lines(vec(k)).point2];
+                plot(xy(:,1),xy(:,2),'LineWidth',2,'Color',color);
+            end
+        end
+        
+    end
+    if ~isempty(circen_1)
+        circ1 = viscircles(circen_1, cirrad_1, 'Color', 'b', 'LineWidth', 1, 'LineStyle', '--');
+    end
+    if ~isempty(circen_2)
+        circ2 = viscircles(circen_2, cirrad_2, 'Color', 'r', 'LineWidth', 1, 'LineStyle', '--');
+    end
+    hold off
+    title('Shapes in different colors')
+    subplot(2,3,5);
+    scatter(thetas,rhos)
+    title('Theta and rho scatter')
+    h=subplot(2,3,6); imshow(pixelCandidates,'Parent',h),hold on
+    for zz = 1:size(windowCandidates,1)
+        w=windowCandidates;
+        r=rectangle(h,'Position', [w(zz,1), w(zz,2), w(zz,3), w(zz,4)], 'LineWidth',1,'EdgeColor','b');
+        
+    end
+    title('Final bounding boxes without merge')
+    drawnow
     waitforbuttonpress
+    
+    % Timing function
+    toc
 end
 
-
-else 
-    windowCandidates=[];
-end %end if lines is not empty
-toc
 end
 
 function [closest_ind,end_seg_]=findClosestPoint(lines,pool,ind,end_seg)
@@ -279,11 +277,11 @@ closest_candidates_p2=[];
 %lines=lines(pool);
 for i=-w:1:w
     for j=-w:1:w
-        ref_point=[ref(1)+i,ref(2)+j];%construct 'ref_point' as the exact point to look for inside the window. 
+        ref_point=[ref(1)+i,ref(2)+j];%construct 'ref_point' as the exact point to look for inside the window.
         for l=1:length(pool)
             %if a point1 or point2 in lines match the 'ref_point', a
             %closest_candidate has been found.
-            if isequal(lines(pool(l)).point1,ref_point)  && ind~=l 
+            if isequal(lines(pool(l)).point1,ref_point)  && ind~=l
                 closest_candidates_p1=[closest_candidates_p1; pool(l)];
             elseif  isequal(lines(pool(l)).point2,ref_point) && ind~=l
                 closest_candidates_p2=[closest_candidates_p2; pool(l)];
@@ -319,38 +317,38 @@ closest_ind=min_ind;
 end
 
 function [bb]=getBoundingBox(lines, shape)
-%get bounding box just considering 'lines' with the indexes in 'shape' vector 
+%get bounding box just considering 'lines' with the indexes in 'shape' vector
 max_y=0;
 min_y=1000000000;
 max_x=0;
 min_x=1000000000;
 for i=1:length(shape)
-  point= lines(shape(i)).point1;
-  if (point(1)>max_y)
-      max_y=point(1);
-  end
-  if point(1)<min_y
-      min_y=point(1);
-  end
-  if point(2)>max_x
-      max_x=point(2);
-  end
-  if point(2)<min_x
-      min_x=point(2);
-  end
-   point= lines(shape(i)).point2;
+    point= lines(shape(i)).point1;
     if (point(1)>max_y)
-      max_y=point(1);
+        max_y=point(1);
     end
-  if point(1)<min_y
-      min_y=point(1);
-  end
-  if point(2)>max_x
-      max_x=point(2);
-  end
-  if point(2)<min_x
-      min_x=point(2);
-  end
+    if point(1)<min_y
+        min_y=point(1);
+    end
+    if point(2)>max_x
+        max_x=point(2);
+    end
+    if point(2)<min_x
+        min_x=point(2);
+    end
+    point= lines(shape(i)).point2;
+    if (point(1)>max_y)
+        max_y=point(1);
+    end
+    if point(1)<min_y
+        min_y=point(1);
+    end
+    if point(2)>max_x
+        max_x=point(2);
+    end
+    if point(2)<min_x
+        min_x=point(2);
+    end
 end
 
 bb=[min_y,min_x,max_y-min_y, max_x-min_x];
